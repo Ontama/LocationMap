64c00f99a2d8913ffa40f3cd42ab6e98bdca158d
ontama154 <onchanvvv@gmail.com>
2022-03-22 14:02:19 +0900

    Initial Commit

---
+++ Shared/LocationMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationMapApp.swift
+//  Shared
+//
+//  Created by tomoyo_kageyama on 2022/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
